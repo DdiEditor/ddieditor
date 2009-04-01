bda5575df86f547d0589ecaa54b19d9b0062a237
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-04-01 08:57:41 +0000

    Added test addResorce
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@313 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- testsource/org/ddialliance/ddieditor/persistenceaccess/filesystem/FilesystemMangerTest.java
+++ testsource/org/ddialliance/ddieditor/persistenceaccess/filesystem/FilesystemMangerTest.java
@@ -1,12 +1,10 @@
 package org.ddialliance.ddieditor.persistenceaccess.filesystem;
 
 import java.io.File;
-import java.util.List;
 
 import junit.framework.Assert;
 
 import org.ddialliance.ddieditor.DdieditorTestCase;
-import org.ddialliance.ddieditor.model.resource.DDIResourceDocument;
 import org.ddialliance.ddieditor.persistenceaccess.PersistenceManager;
 import org.junit.Test;
 
@@ -19,22 +17,12 @@ public class FilesystemMangerTest extends DdieditorTestCase {
 		Assert.assertTrue(file.exists());
 	}
 	
+	@Test
 	public void addResorce() throws Exception {
-		// delete
-		String id = SINGLE_MANINTAINABLE_QS_FD_NS_DOC;
-		int index = id.indexOf(".");
-		id = id.substring(index)+".dbxml";
-		
-		// readd
-		PersistenceManager.getInstance().deleteResource(id);
-		FilesystemManager.getInstance().addResource(
-				new File("resources" + File.separator
-						+ SINGLE_MANINTAINABLE_QS_FD_NS_DOC));
-		
-		// query
-		List<DDIResourceDocument> list = PersistenceManager.getInstance().getResourceById(id);
-		if (list.isEmpty()) {
-			Assert.fail(id+" not refound!");
+		try {
+			FilesystemManager.getInstance().addResource(new File("resources"+File.separator+SINGLE_MANINTAINABLE_QS_FD_NS_DOC));
+		} catch (Exception e) {
+			Assert.fail();
 		}
 	}
 }
