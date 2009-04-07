cd38e05f7c8ab5d894fc6b3dba3f67fc46e1e0fa
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-04-07 09:45:43 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@326 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- testsource/org/ddialliance/ddieditor/persistenceaccess/namespace/ddi3/Ddi3NamespaceGeneratorTest.java
+++ testsource/org/ddialliance/ddieditor/persistenceaccess/namespace/ddi3/Ddi3NamespaceGeneratorTest.java
@@ -12,18 +12,4 @@ public class Ddi3NamespaceGeneratorTest {
 		Ddi3NamespacePrefix prefix = gen.getNamespaceObjectByElement("DDIInstance");
 		Assert.assertEquals(Ddi3NamespacePrefix.INSTANCE, prefix);
 	}
-
-	@Test
-	public void substitutePrefixesFromElements() throws Exception {
-		Ddi3NamespaceGenerator gen = new Ddi3NamespaceGenerator();
-		String query = "<l:Variable id=\"v5001\"><r:Label>SKEMATYPE1</r:Label>"
-				+ "<l:ConceptReference><r:ID>cs_2</r:ID></l:ConceptReference>"
-				+ "<l:QuestionReference><r:ID>qi_5</r:ID></l:QuestionReference></l:Variable>";
-
-		query = gen.substitutePrefixesFromElements(query);
-		Assert
-				.assertEquals(
-						"<Variable xmlns=\"ddi:logicalproduct:3_0\" id=\"v5001\"><Label xmlns=\"ddi:reusable:3_0\">SKEMATYPE1</Label><ConceptReference xmlns=\"ddi:datacollection:3_0\"><ID xmlns=\"ddi:reusable:3_0\">cs_2</ID></ConceptReference><QuestionReference xmlns=\"ddi:datacollection:3_0\"><ID xmlns=\"ddi:reusable:3_0\">qi_5</ID></QuestionReference></Variable>",
-						query);
-	}
 }
