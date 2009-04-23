5074c8777c428916133fff7e27198befee8d530a
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-04-23 09:46:21 +0000

    Added support for: queryScheme Plus minor updates
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@347 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- testsource/org/ddialliance/ddieditor/model/DdiManagerTest.java
+++ testsource/org/ddialliance/ddieditor/model/DdiManagerTest.java
@@ -3,6 +3,8 @@ package org.ddialliance.ddieditor.model;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.ddialliance.ddi_3_0.xml.xmlbeans.conceptualcomponent.ConceptDocument;
 import org.ddialliance.ddi_3_0.xml.xmlbeans.datacollection.DataCollectionDocument;
@@ -15,6 +17,10 @@ import org.ddialliance.ddi_3_0.xml.xmlbeans.reusable.InternationalStringType;
 import org.ddialliance.ddieditor.DdieditorTestCase;
 import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectListDocument;
 import org.ddialliance.ddieditor.persistenceaccess.PersistenceManager;
+import org.ddialliance.ddieditor.persistenceaccess.SchemeQuery;
+import org.ddialliance.ddieditor.persistenceaccess.SchemeQueryResult;
+import org.ddialliance.ddieditor.persistenceaccess.SchemeUpdateElement;
+import org.ddialliance.ddieditor.persistenceaccess.dbxml.DbXmlManager;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -241,21 +247,24 @@ public class DdiManagerTest extends DdieditorTestCase {
 
 	@Test
 	public void updateElement() throws Exception {
+		PersistenceManager.getInstance().setWorkingResource(
+				DdieditorTestCase.SINGLE_MANINTAINABLE_QS_FD_NS_DOC);
+		
 		String id = "qi_1";
 		String newId = "new_id";
 		String parentId = "qs_";
 		QuestionItemDocument test = DdiManager.getInstance().getQuestionItem(
-				id, null, parentId, null);
+				id, null, parentId, "1.0");
 
-		Assert.assertNotNull(test);
-		Assert.assertEquals(id, test.getQuestionItem().getId());
+		Assert.assertNotNull("Not found!", test);
+		Assert.assertEquals("Id not equal!", id, test.getQuestionItem().getId());
 
 		test.getQuestionItem().setId(newId);
 		DdiManager.getInstance().updateElement(test, id, null);
 		PersistenceManager.getInstance().commitWorkingResource();
 
 		test = DdiManager.getInstance().getQuestionItem(newId, null, parentId,
-				null);
+				"1.0");
 		Assert.assertNotNull(test);
 		Assert.assertEquals(newId, test.getQuestionItem().getId());
 	}
@@ -273,5 +282,94 @@ public class DdiManagerTest extends DdieditorTestCase {
 
 		DdiManager.getInstance().deleteElement(test, parentId, null,
 				"datacollection__DataCollection");
+	}
+
+	@Test
+	public void queryScheme() throws Exception {
+		PersistenceManager.getInstance().setWorkingResource(
+				DdieditorTestCase.FULLY_DECLARED_NS_DOC);
+
+		// query
+		SchemeQueryResult result = DdiManager.getInstance().getQuestionSchemeLabel("qs_", null, "dd", null);
+		Assert.assertEquals("qs_", result.getId());
+		Assert.assertEquals(2, result.getElements()[0].size());
+
+		// crud setup
+		result.cleanElements();
+		SchemeUpdateElement update = new SchemeUpdateElement();
+		update.setLocalName("Label");
+		String updateValue = "godspeed";
+		update.setValue("<r:Label xml:lang='da'>" + updateValue + "</r:Label>");
+		update.setUpdateValue(1);
+
+		// update
+		List<SchemeUpdateElement> elements = new ArrayList<SchemeUpdateElement>();
+		elements.add(update);
+		DdiManager.getInstance().updateSchema(result, elements);
+		PersistenceManager.getInstance().commitWorkingResource();
+		SchemeQueryResult test = DdiManager.getInstance().getQuestionSchemeLabel("qs_", null, "dd", null);
+		Assert.assertTrue("Not updated!", test.getElements()[0].get(0).indexOf(
+				updateValue) > -1);
+
+		// new
+		elements.clear();
+		update.setUpdateValue(SchemeUpdateElement.NEW);
+		update.setValue("<r:Label xml:lang='en'>my new value</r:Label>");
+		elements.add(update);
+		DdiManager.getInstance().updateSchema(result, elements);
+		PersistenceManager.getInstance().commitWorkingResource();
+		test = DdiManager.getInstance().getQuestionSchemeLabel("qs_", null, "dd", null);
+		Assert.assertEquals("New not inserted!", 3, test.getElements()[0]
+				.size());
+
+		// delete
+		elements.clear();
+		update.setUpdateValue(-3);
+		elements.add(update);
+		DdiManager.getInstance().updateSchema(result, elements);
+		PersistenceManager.getInstance().commitWorkingResource();
+		test = DdiManager.getInstance().getQuestionSchemeLabel("qs_", null, "dd", null);
+		Assert.assertEquals("Not deleted!", 2, test.getElements()[0].size());
+	}
+
+	@Test
+	public void querySchemeNoLabel() throws Exception {
+		PersistenceManager.getInstance().setWorkingResource(
+				DdieditorTestCase.FULLY_DECLARED_NS_DOC);
+
+		// test setup
+		String query = "for $element in doc('dbxml:/big-doc.dbxml/big-doc.xml')//*[namespace-uri()='ddi:logicalproduct:3_0' and local-name()='LogicalProduct'] for $child in $element//*[namespace-uri()='ddi:logicalproduct:3_0' and local-name()='CategoryScheme']  where $element/@id = 'lp_1' and $child/@id = 'cats_7' and empty($child/@version) return $child";
+		SchemeQuery schemeQuery = new SchemeQuery();
+		schemeQuery.setQuery(query);
+		schemeQuery.setElementNames(new String[] { "reusable__Label" });
+		schemeQuery.setSchemeTarget("CategoryScheme");
+		schemeQuery.setStopTag("Category");
+
+		// result
+		SchemeQueryResult result = DdiManager.getInstance().queryScheme(
+				schemeQuery);
+		Assert.assertEquals("cats_7", result.getId());
+		Assert.assertEquals(0, result.getElements()[0].size());
+
+		// crud setup
+		result.cleanElements();
+		SchemeUpdateElement update = new SchemeUpdateElement();
+		update.setLocalName("Label");
+		String updateValue = "godspeed";
+		update.setValue("<r:Label xml:lang='da'>" + updateValue + "</r:Label>");
+		update.setUpdateValue(0);
+
+		// new
+		List<SchemeUpdateElement> elements = new ArrayList<SchemeUpdateElement>();
+		elements.clear();
+		update.setUpdateValue(SchemeUpdateElement.NEW);
+		update.setValue("<r:Label xml:lang='en'>my new value</r:Label>");
+		elements.add(update);
+		DdiManager.getInstance().updateSchema(result, elements);
+		PersistenceManager.getInstance().commitWorkingResource();
+		SchemeQueryResult test = DdiManager.getInstance().queryScheme(
+				schemeQuery);
+		Assert.assertEquals("New not inserted!", 1, test.getElements()[0]
+				.size());
 	}
 }
