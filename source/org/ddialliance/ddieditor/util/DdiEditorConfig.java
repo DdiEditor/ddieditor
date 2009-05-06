18e8c31476c5047fc36745487c88163a67f92f8e
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-05-06 09:57:57 +0000

    Added dbxml.import.validate
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@423 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- source/org/ddialliance/ddieditor/util/DdiEditorConfig.java
+++ source/org/ddialliance/ddieditor/util/DdiEditorConfig.java
@@ -7,6 +7,7 @@ import org.ddialliance.ddiftp.util.log.LogType;
 public class DdiEditorConfig extends Config {
 	// dbxml
 	public static final String DBXML_ENVIROMENT_HOME = "dbxml.enviroment.home";
+	public static final String DBXML_IMPORT_VALIDATE = "dbxml.import.validate";
 	public static final String DDI_ELEMENTS_RELATIONSHIP_LIST = "ddi.elements.relationshiplist";
 	
 	public static void init() {
