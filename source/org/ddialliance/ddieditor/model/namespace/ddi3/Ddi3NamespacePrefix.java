da20d6b9176d0f9add4e6a4212656538d4bd57b8
Jannik Vestergaard Jensen <jvj@dda.dk>
2009-10-30 13:12:43 +0000

    Minor update
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@720 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- source/org/ddialliance/ddieditor/model/namespace/ddi3/Ddi3NamespacePrefix.java
+++ source/org/ddialliance/ddieditor/model/namespace/ddi3/Ddi3NamespacePrefix.java
@@ -5,43 +5,43 @@ package org.ddialliance.ddieditor.model.namespace.ddi3;
  * 
  * <p>
  * xsi, http://www.w3.org/2001/XMLSchema-instance<br>
- * ddi, ddi:instance:3_0<br>
- * r, ddi:reusable:3_0<br>
+ * ddi, ddi:instance:3_1<br>
+ * r, ddi:reusable:3_1<br>
  * xhtml, http://www.w3.org/1999/xhtml<br>
- * dce, ddi:dcelements:3_0<br>
+ * dce, ddi:dcelements:3_1<br>
  * dc, http://purl.org/dc/elements/1.1/<br>
- * a, ddi:archive:3_0<br>
- * g, ddi:group:3_0<br>
- * cm, ddi:comparative:3_0<br>
- * c, ddi:conceptualcomponent:3_0<br>
- * d, ddi:datacollection:3_0<br>
- * l, ddi:logicalproduct:3_0<br>
- * pd, ddi:physicaldataproduct:3_0<br>
- * ds, ddi:dataset:3_0<br>
- * pi, ddi:physicalinstance:3_0<br>
- * m1", "ddi:physicaldataproduct/ncube/normal:3_0<br>
- * m2", "ddi:physicaldataproduct/ncube/tabular:3_0<br>
- * m3", "ddi:physicaldataproduct/ncube/inline:3_0<br>
- * s, ddi:studyunit:3_0<br>
- * pr, ddi:profile:3_0
+ * a, ddi:archive:3_1<br>
+ * g, ddi:group:3_1<br>
+ * cm, ddi:comparative:3_1<br>
+ * c, ddi:conceptualcomponent:3_1<br>
+ * d, ddi:datacollection:3_1<br>
+ * l, ddi:logicalproduct:3_1<br>
+ * pd, ddi:physicaldataproduct:3_1<br>
+ * ds, ddi:dataset:3_1<br>
+ * pi, ddi:physicalinstance:3_1<br>
+ * m1", "ddi:physicaldataproduct/ncube/normal:3_1<br>
+ * m2", "ddi:physicaldataproduct/ncube/tabular:3_1<br>
+ * m3", "ddi:physicaldataproduct/ncube/inline:3_1<br>
+ * s, ddi:studyunit:3_1<br>
+ * pr, ddi:profile:3_1
  * </p>
  */
 public enum Ddi3NamespacePrefix {
 	SCHEMA_INSTANCE("xsi", "http://www.w3.org/2001/XMLSchema-instance"), INSTANCE(
-			"ns1", "ddi:instance:3_0"), REUSEABLE("r", "ddi:reusable:3_0"), XHTML(
+			"ns1", "ddi:instance:3_1"), REUSEABLE("r", "ddi:reusable:3_1"), XHTML(
 			"xhtml", "http://www.w3.org/1999/xhtml"), DC_ELEMENTS("dce",
-			"ddi:dcelements:3_0"), DUBLIN_CORE("dc",
-			"http://purl.org/dc/elements/1.1/"), ARCHIVE("a", "ddi:archive:3_0"), GROUP(
-			"g", "ddi:group:3_0"), COMPARATIVE("cm", "ddi:comparative:3_0"), CONCEPTUAL_COMPONENTS(
-			"c", "ddi:conceptualcomponent:3_0"), DATA_COLLECTION("d",
-			"ddi:datacollection:3_0"), LOGIAL_PRODUCT("l",
-			"ddi:logicalproduct:3_0"), PHYSICAL_DATA_PRODUCT("pd",
-			"ddi:physicaldataproduct:3_0"), DATA_SET("ds", "ddi:dataset:3_0"), PHYSICAL_INSTANCE(
-			"pi", "ddi:physicalinstance:3_0"), NCUBE_NORMAL("m1",
-			"ddi:physicaldataproduct/ncube/normal:3_0"), NCUBE_TABULAR("m2",
-			"ddi:physicaldataproduct/ncube/tabular:3_0"), NCUBE_INLINE("m3",
-			"ddi:physicaldataproduct/ncube/inline:3_0"), STUDY_UNIT("s",
-			"ddi:studyunit:3_0"), PROFILE("pr", "ddi:profile:3_0");
+			"ddi:dcelements:3_1"), DUBLIN_CORE("dc",
+			"http://purl.org/dc/elements/1.1/"), ARCHIVE("a", "ddi:archive:3_1"), GROUP(
+			"g", "ddi:group:3_1"), COMPARATIVE("cm", "ddi:comparative:3_1"), CONCEPTUAL_COMPONENTS(
+			"c", "ddi:conceptualcomponent:3_1"), DATA_COLLECTION("d",
+			"ddi:datacollection:3_1"), LOGIAL_PRODUCT("l",
+			"ddi:logicalproduct:3_1"), PHYSICAL_DATA_PRODUCT("pd",
+			"ddi:physicaldataproduct:3_1"), DATA_SET("ds", "ddi:dataset:3_1"), PHYSICAL_INSTANCE(
+			"pi", "ddi:physicalinstance:3_1"), NCUBE_NORMAL("m1",
+			"ddi:physicaldataproduct/ncube/normal:3_1"), NCUBE_TABULAR("m2",
+			"ddi:physicaldataproduct/ncube/tabular:3_1"), NCUBE_INLINE("m3",
+			"ddi:physicaldataproduct/ncube/inline:3_1"), STUDY_UNIT("s",
+			"ddi:studyunit:3_1"), PROFILE("pr", "ddi:profile:3_1");
 
 	public static final String DEFAULT_NAMESPACE_URI = "http://www.ddialliance.org/DDI/schema/ddi3.0/instance.xsd";
 	private String prefix;
@@ -96,7 +96,7 @@ public enum Ddi3NamespacePrefix {
 		result.append("\"; ");
 		return result.toString();
 	}
-	
+
 	public static Ddi3NamespacePrefix getNamespaceByDefaultPrefix(String prefix) {
 		for (int i = 0; i < Ddi3NamespacePrefix.values().length; i++) {
 			Ddi3NamespacePrefix namespacePrefix = Ddi3NamespacePrefix.values()[i];
@@ -105,5 +105,16 @@ public enum Ddi3NamespacePrefix {
 			}
 		}
 		return null;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		result.append(this.name());
+		result.append(", prefix: ");
+		result.append(prefix);
+		result.append(", namespace: ");
+		result.append(namespace);
+		return result.toString();
 	}
 }
