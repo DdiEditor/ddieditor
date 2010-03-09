251626fcd4ea9fc8381a581a43d4d82d52eb5adf
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-03-09 10:18:56 +0000

    Minor updates
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@999 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- testsource/org/ddialliance/ddieditor/model/namespace/ddi3/Ddi3NamespaceHelperTest.java
+++ testsource/org/ddialliance/ddieditor/model/namespace/ddi3/Ddi3NamespaceHelperTest.java
@@ -1,31 +1,40 @@
 package org.ddialliance.ddieditor.model.namespace.ddi3;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.xmlbeans.XmlObject;
+import org.ddialliance.ddieditor.DdieditorTestCase;
 import org.ddialliance.ddieditor.model.DdiManager;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class Ddi3NamespaceHelperTest {
+public class Ddi3NamespaceHelperTest extends DdieditorTestCase {
 	@Test
 	public void namespaceLookup() throws Exception {
 		String namespace = null;
+		Ddi3NamespacePrefix ddi3NamespacePrefix;
 		String element = null;
 		List<String> errorList = new ArrayList<String>();
 
 		// create indices for identifiables
 		for (Iterator<Object> iterator = DdiManager.getInstance()
-				.getDdi3NamespaceHelper().getIdentifiables().keySet().iterator(); iterator
-				.hasNext();) {
+				.getDdi3NamespaceHelper().getIdentifiables().keySet()
+				.iterator(); iterator.hasNext();) {
 			element = (String) iterator.next();
 
 			// check for missing elements in element ~ namespace list
 			try {
-				namespace = DdiManager.getInstance().getDdi3NamespaceHelper()
-						.getNamespaceObjectByElement(element).getNamespace();
+				ddi3NamespacePrefix = DdiManager.getInstance()
+						.getDdi3NamespaceHelper().getNamespaceObjectByElement(
+								element);
+				namespace = ddi3NamespacePrefix.getNamespace();
+				System.out.println(ddi3NamespacePrefix);
 			} catch (Exception e) {
+				e.printStackTrace();
 				errorList.add(element);
 				continue;
 			}
@@ -38,5 +47,23 @@ public class Ddi3NamespaceHelperTest {
 		}
 		Assert.assertTrue("Namespacelookup error on: " + errorStr.toString(),
 				errorList.isEmpty());
+	}
+
+	// TODO:
+	// Namespacelookup error on: physicaldataproduct_ncube_inline__RecordLayout,
+	// physicaldataproduct_ncube_inline__NCubeInstance,
+	// physicaldataproduct_ncube_tabular__NCubeInstance,
+	// ProprietaryRecordLayout, DDIProfile,
+	// physicaldataproduct_ncube_normal__NCubeInstance,
+	// physicaldataproduct_ncube_tabular__RecordLayout,
+	// physicaldataproduct_ncube_normal__RecordLayout,
+
+	@Test
+	public void isMaintainalbel() throws Exception {
+		DdiManager.getInstance().setWorkingDocument(FULLY_DECLARED_NS_DOC);
+		XmlObject xmlObject = DdiManager.getInstance().getConceptScheme("cs",
+				null, null, null);
+		Assert.assertTrue("Not !", DdiManager.getInstance()
+				.getDdi3NamespaceHelper().isMaintainable(xmlObject));
 	}
 }
