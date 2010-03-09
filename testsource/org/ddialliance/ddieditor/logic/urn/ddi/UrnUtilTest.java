2904c7a537a201df8400d40080cbaccb7c843106
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-03-09 10:18:28 +0000

    Minor updates
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@998 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- testsource/org/ddialliance/ddieditor/logic/urn/ddi/UrnUtilTest.java
+++ testsource/org/ddialliance/ddieditor/logic/urn/ddi/UrnUtilTest.java
@@ -14,29 +14,12 @@ public class UrnUtilTest extends DdieditorTestCase {
 	public void getUrn() throws Exception {
 		PersistenceManager.getInstance().setWorkingResource(
 				DdieditorTestCase.FULLY_DECLARED_NS_DOC);
-
-		String urn2 = "urn:ddi:3.0:DataCollection=dda.dk:dd[1.0]";
 		Urn urn = Urn2Util.getUrn("datacollection__DataCollection", "dd", null, null, null);
 		
 		urn = Urn2Util.getUrn("QuestionItem", "qi_1", "", "qs_", "");
 		Assert.assertEquals(
-				"urn:ddi:3.0:QuestionItem=dda.dk:dda-4755(1.0).qi_1(1.0)", urn
+				"urn:ddi:dda.dk.ddi:QuestionScheme.qs_.1.0.0:QuestionItem.qi_1.1.0.0", urn
 						.toUrnString());
-
-		urn = Urn2Util.getUrn("QuestionItem", "qi_1", "", "qs_-2", "1.0.1");
-		Assert.assertEquals(
-				"urn:ddi:3.0:QuestionItem=dda.dk:qs_-2(1.0.1).qi_1(1.0.1)", urn
-						.toUrnString());
-
-		urn = Urn2Util.getUrn("QuestionItem", "qi_1", "", "qs_-2", "1.0.2");
-		Assert.assertEquals(
-				"urn:ddi:3.0:QuestionItem=hungobongo:qs_-2(1.0.2).qi_1(1.0.2)",
-				urn.toUrnString());
-
-		urn = Urn2Util.getUrn("QuestionItem", "qi_1", "3.0", "qs_-2", "1.0.2");
-		Assert.assertEquals(
-				"urn:ddi:3.0:QuestionItem=hungobongo:qs_-2(1.0.2).qi_1(3.0)",
-				urn.toUrnString());
 
 		urn = Urn2Util.getUrn("DDIInstance", "dda-4755", "1.0", null, null);
 		Assert.assertEquals("urn:ddi:3.0:DDIInstance=dda.dk:dda-4755(1.0)", urn
