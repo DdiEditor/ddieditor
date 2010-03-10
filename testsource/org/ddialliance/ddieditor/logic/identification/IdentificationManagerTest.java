63d459f4515a9ba6418471262055d53abe26b7b2
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-03-10 12:08:42 +0000

    Added test getVersionInformation
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@1007 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- testsource/org/ddialliance/ddieditor/logic/identification/IdentificationManagerTest.java
+++ testsource/org/ddialliance/ddieditor/logic/identification/IdentificationManagerTest.java
@@ -1,13 +1,10 @@
 package org.ddialliance.ddieditor.logic.identification;
 
-import static org.junit.Assert.*;
-
 import org.apache.xmlbeans.XmlOptions;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.DataCollectionDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.datacollection.DataCollectionType;
-import org.ddialliance.ddi3.xml.xmlbeans.logicalproduct.CodeSchemeReferenceDocument;
+import org.ddialliance.ddi3.xml.xmlbeans.datacollection.InstrumentDocument;
 import org.ddialliance.ddi3.xml.xmlbeans.reusable.ReferenceType;
-import org.ddialliance.ddieditor.logic.identification.IdentificationManager;
 import org.ddialliance.ddieditor.logic.identification.IdentificationManager.VersionChangeType;
 import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
 import org.junit.Test;
@@ -43,14 +40,24 @@ public class IdentificationManagerTest {
 		ops.put(XmlOptions.SAVE_PRETTY_PRINT);
 		System.out.println(ref.xmlText(ops));
 
-//		CodeSchemeReferenceDocument doc = CodeSchemeReferenceDocument.Factory
-//				.newInstance();
-//		doc.addNewCodeSchemeReference();
-//		doc.getCodeSchemeReference().addNewID();
-//		doc.getCodeSchemeReference().getIDList().get(0).setStringValue(
-//				"some-id");
-//
-//		System.out.println(doc.getCodeSchemeReference().xmlText(ops));
-//		System.out.println(doc.xmlText(ops));
+		// CodeSchemeReferenceDocument doc = CodeSchemeReferenceDocument.Factory
+		// .newInstance();
+		// doc.addNewCodeSchemeReference();
+		// doc.getCodeSchemeReference().addNewID();
+		// doc.getCodeSchemeReference().getIDList().get(0).setStringValue(
+		// "some-id");
+		//
+		// System.out.println(doc.getCodeSchemeReference().xmlText(ops));
+		// System.out.println(doc.xmlText(ops));
+	}
+
+	@Test
+	public void getVersionInformation() throws Exception {
+		String xml = "<Instrument xmlns=\"ddi:datacollection:3_1\" id=\"inst-1268157656095\" agency=\"dk.dda\" version=\"1.0.0\" versionDate=\"2010-03-09T19:00:56.099+01:00\" >"
+				+ "<ddi1:VersionResponsibility xmlns:ddi1=\"ddi:reusable:3_1\">ddajvj</ddi1:VersionResponsibility>"
+				+ "<ddi1:VersionRationale translated=\"false\" translatable=\"true\" xml:lang=\"DK\" xmlns:ddi1=\"ddi:reusable:3_1\">Version: 1.0.0, date: 2010-03-09T19:00:56.099+01:00</ddi1:VersionRationale>"
+				+ "</Instrument>";
+		InstrumentDocument doc = InstrumentDocument.Factory.parse(xml);
+		IdentificationManager.getInstance().getVersionInformation(doc);
 	}
 }
