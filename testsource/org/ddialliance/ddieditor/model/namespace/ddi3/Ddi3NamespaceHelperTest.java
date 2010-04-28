b1a0cf757bfcf2319dc5773cc2796a0041fa5886
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-04-28 09:29:25 +0000

    Added tests for substitute prefixes
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@1160 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- testsource/org/ddialliance/ddieditor/model/namespace/ddi3/Ddi3NamespaceHelperTest.java
+++ testsource/org/ddialliance/ddieditor/model/namespace/ddi3/Ddi3NamespaceHelperTest.java
@@ -1,15 +1,16 @@
 package org.ddialliance.ddieditor.model.namespace.ddi3;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.xmlbeans.XmlObject;
+import org.ddialliance.ddi3.xml.xmlbeans.datacollection.DataCollectionDocument;
+import org.ddialliance.ddi3.xml.xmlbeans.datacollection.QuestionSchemeDocument;
 import org.ddialliance.ddieditor.DdieditorTestCase;
 import org.ddialliance.ddieditor.model.DdiManager;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class Ddi3NamespaceHelperTest extends DdieditorTestCase {
@@ -65,5 +66,38 @@ public class Ddi3NamespaceHelperTest extends DdieditorTestCase {
 				null, null, null);
 		Assert.assertTrue("Not !", DdiManager.getInstance()
 				.getDdi3NamespaceHelper().isMaintainable(xmlObject));
+	}
+
+	@Test
+	public void getDuplicateConvention() throws Exception {
+		QuestionSchemeDocument qsDoc = QuestionSchemeDocument.Factory
+				.newInstance();
+		String result = DdiManager.getInstance().getDdi3NamespaceHelper()
+				.getDuplicateConvention(
+						qsDoc.schemaType().getDocumentElementName());
+
+		Assert.assertEquals("QuestionScheme", result);
+
+		DataCollectionDocument dcDoc = DataCollectionDocument.Factory
+				.newInstance();
+		result = DdiManager.getInstance().getDdi3NamespaceHelper()
+				.getDuplicateConvention(
+						dcDoc.schemaType().getDocumentElementName());
+
+		Assert.assertEquals("datacollection__DataCollection", result);
+	}
+
+	@Test
+	public void substitutePrefixesFromElements() throws Exception {
+		String xml = "<ddi:QuestionItem id=\"quei-1271925620862\" version=\"1.0.0\" versionDate=\"2010-04-22T10:40:20.864+02:00\" xmlns:ddi=\"ddi:datacollection:3_1\"><ddi1:VersionResponsibility xmlns:ddi1=\"ddi:reusable:3_1\">ddajvj</ddi1:VersionResponsibility><ddi1:VersionRationale translated=\"false\" translatable=\"true\" xml:lang=\"DK\" xmlns:ddi1=\"ddi:reusable:3_1\">Version: 1.0.0, date: 2010-04-20T15:50:23.586+02:00</ddi1:VersionRationale><ddi:ConceptReference><ddi1:ID xmlns:ddi1=\"ddi:reusable:3_1\"/></ddi:ConceptReference></ddi:QuestionItem>";
+		String result = DdiManager.getInstance().getDdi3NamespaceHelper()
+				.substitutePrefixesFromElements(xml);
+	}
+	
+	@Test
+	public void substitutePrefixesFromEmtyElement() throws Exception {
+		String xml = "<ddi1:ID xmlns:ddi1=\"ddi:reusable:3_1\"/>";
+		String result = DdiManager.getInstance().getDdi3NamespaceHelper()
+				.substitutePrefixesFromElements(xml);
 	}
 }
