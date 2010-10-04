992a3f02bfa3fc71e0113da369ecccfa9fdf4500
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-10-04 06:27:08 +0000

    Initial import
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@1697 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

---
+++ source/org/ddialliance/ddieditor/logic/urn/ddi/ReferenceResolution.java
@@ -0,0 +1,57 @@
+package org.ddialliance.ddieditor.logic.urn.ddi;
+
+import org.ddialliance.ddi3.xml.xmlbeans.reusable.ReferenceType;
+import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
+
+/**
+ * Utility to access to reference information. Wrapping model, scheme, URN or id
+ * reference
+ */
+public class ReferenceResolution {
+	String id;
+	ReferenceType ref;
+
+	public ReferenceResolution(String id) {
+		super();
+		this.id = id;
+	}
+
+	public ReferenceResolution(ReferenceType ref) {
+		this.ref = ref;
+
+		// TODO urn
+
+		// id
+		if (ref.getIDArray(0) != null) {
+			id = ref.getIDArray(0).getStringValue();
+		}
+	}
+
+	public ReferenceResolution(LightXmlObjectType lightXmlObject) {
+		ref = ReferenceType.Factory.newInstance();
+		
+		// agency 
+		// TODO ref.addIdentifyingAgency(lightXmlObject.getAgency());
+		
+		// id
+		ref.addNewID().setStringValue(lightXmlObject.getId());
+		
+		// version
+		if (lightXmlObject.getVersion() != null
+				&& !lightXmlObject.getVersion().equals("")) {
+			ref.addNewVersion().setStringValue(lightXmlObject.getVersion());
+		}
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public ReferenceType getReference() {
+		return ref;
+	}
+}
