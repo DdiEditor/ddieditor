c84b612fd749713c7b42ae06dbfe42c3a2322def
Jannik Vestergaard Jensen <jvj@dda.dk>
2010-11-01 08:18:26 +0000

    Added variable support
    git-svn-id: svn://192.168.99.101/dda/trunk/ddiftp/ddieditor@1862 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- source/org/ddialliance/ddieditor/model/conceptual/ConceptualType.java
+++ source/org/ddialliance/ddieditor/model/conceptual/ConceptualType.java
@@ -1,5 +1,5 @@
 package org.ddialliance.ddieditor.model.conceptual;
 
 public enum ConceptualType {
-	STUDY, LOGIC_Universe, LOGIC_concepts, LOGIC_category, LOGIC_code, LOGIC_questions, LOGIC_instumentation;
+	STUDY, LOGIC_Universe, LOGIC_concepts, LOGIC_category, LOGIC_code, LOGIC_questions, LOGIC_instumentation, LOGIC_variable;
 }
