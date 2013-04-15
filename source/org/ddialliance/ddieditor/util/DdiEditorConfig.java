64636f3e122b1b738735ff0b66dac79d50d02ee0
Jannik Vestergaard Jensen <jvj@dda.dk>
2013-04-15 07:38:19 +0000

    Added utf-8 bom property
    git-svn-id: svn://192.168.99.101/dda/branches/ddieditor/corranhorn/ddiftp/ddieditor@4410 47c8fc04-09cc-d246-8e9e-e5ba08b125bf

--- source/org/ddialliance/ddieditor/util/DdiEditorConfig.java
+++ source/org/ddialliance/ddieditor/util/DdiEditorConfig.java
@@ -28,6 +28,7 @@ public class DdiEditorConfig extends Config {
 	public static final String SPPS_OMS_XML_NAMESPACE = "spss.oms.xml.namespace";
 	public static final String SPSS_IMPORT_CHARSET="spss.import.charset";
 	public static final String SPSS_IMPORT_DECIMAL_SEPERATOR="spss.import.decimalseparator";
+	public static final String UTF8_ADD_BOM="charset_utf8_add_bom";
 	public static final String OSIRIS_TO_DDIL_TITLE_PARTH_SPLIT="osiristoddil.title.pathsplit";
 		
 	public static void init() {
